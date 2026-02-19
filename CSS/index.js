ce00a353d44b0d38881df1a6b85a95b0c68aa673
Justina <justinalin92@gamil.com>
2026-02-19 08:00:57 -0600

    me

--- CSS/index.js
+++ CSS/index.js
@@ -1,19 +1,28 @@
 index.js
-// 1. Create a date object for today
 const today = new Date();
-
-// 2. Get the current year from the date object
 const thisYear = today.getFullYear();
-
-// 3. Select the footer element from the DOM
 const footer = document.querySelector('footer');
-
-// 4. Create a new paragraph element
 const copyright = document.createElement('p');
-
-// 5. Set the inner HTML with your name, copyright symbol, and current year
-// Replace "Your Name" with your actual name
 copyright.innerHTML = `\u00A9 Your Name ${thisYear}`;
-
-// 6. Append the paragraph to the footer
+<section> <h2>Leave a Message</h2>
+  <form id="leave_message">
+    <label> Username:
+      <input type="text" name="username" required> </label>
+<label>Email:
+      <input type="email" name="usersEmail" required>
+    </label>
+<label>Message:<textarea name="message" required></textarea>
+    </label>
+<button type="submit">Leave Message </button>
+</form>
+</section>
 footer.appendChild(copyright);
+<section>
+    </section>const messageSection
+messageSection
+const newMessage
+<a></a>
+const removeButton
+<ul>
+    
+</ul>
